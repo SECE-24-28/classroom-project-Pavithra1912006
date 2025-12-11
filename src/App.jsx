04e22a5cd7e0c16b9b6c0821730d4a5660b9bfe5
Pavithra1912006 <pavithra1912006@gmail.com>
2025-12-11 10:01:44 +0530

    Changes made in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,98 @@
-/*import "./App.css";
-import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+// /*import "./App.css";
+// import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
 
-function App() {
-  return (
-    <div>
-      {<HelloWorldComponent /> }
-      {<FlexBoxComponent /> }
-      <LeadersTomorrowComponent />
+// function App() {
+//   return (
+//     <div>
+//       {<HelloWorldComponent /> }
+//       {<FlexBoxComponent /> }
+//       <LeadersTomorrowComponent />
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
-*/
+// */
+
+// import React from "react";
+// // import FlexBoxComponent from "./flex/flex-box-component";
+// //import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
+// import RectangleBox from "./rectangle-box/rectangle-box-component";
+// import Mycomponent from "./Galance/galance-component";
+// import ContactSection from "./Contact/ContactSection";
+// import OurPromisesComponent from "./our-promises/our-promises";
+// import TrustedbyComponent from "./trusted-by/trusted-by-component";
+// //import College from "./college/college-compoent.jx";
+// import College from "./college/college-component.jsx";
+// import NotFound from "./NotFound";
+// import { Routes, Route } from 'react-router-dom';
+// // import UsestateformComponent from "./usestate/use-state-component.jsx";   
+// import Lms from "./usestate/use-state-component";
+// import FetchApiComponent from "./fetch-api/fetch-api-component";
+// import UseEffectPracticeTwo from "./use-effect-practice/use-effect-practice-2";
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<RectangleBox />}/>
+//       <Route path="/galance" element={<Mycomponent/>}/>
+//       <Route path="/trusted-by" element={<TrustedbyComponent />}/>
+//       <Route path="/our-promises" element={<OurPromisesComponent />}/>
+//       <Route path="/contact" element={<ContactSection />}/>
+//       <Route path="/college" element={<College/>}/>
+//       <Route path="*" element={<NotFound />}/>
+//     </Routes>
+//     //  <div>
+//     //   <UsestateformComponent />
+
+//     // </div>
+//     // <div>
+//     //   <Lms/>
+//     // </div>
+//     <Routes>
+//       <Route path="/api-fetch" element={<FetchApiComponent />} />
+//       <Route path="/use-effect-practice-2" element={<UseEffectPracticeTwo />} />
+//     </Routes>
+    
+       
+//   );
+// } 
+
+// export default App;
+
 
 import React from "react";
-// import FlexBoxComponent from "./flex/flex-box-component";
-//import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
 import RectangleBox from "./rectangle-box/rectangle-box-component";
 import Mycomponent from "./Galance/galance-component";
 import ContactSection from "./Contact/ContactSection";
 import OurPromisesComponent from "./our-promises/our-promises";
 import TrustedbyComponent from "./trusted-by/trusted-by-component";
-//import College from "./college/college-compoent.jx";
 import College from "./college/college-component.jsx";
 import NotFound from "./NotFound";
-import { Routes, Route } from 'react-router-dom';
-// import UsestateformComponent from "./usestate/use-state-component.jsx";   
-import Lms from "./usestate/use-state-component";
 import FetchApiComponent from "./fetch-api/fetch-api-component";
 import UseEffectPracticeTwo from "./use-effect-practice/use-effect-practice-2";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    // <Routes>
-    //   <Route path="/" element={<RectangleBox />}/>
-    //   <Route path="/galance" element={<Mycomponent/>}/>
-    //   <Route path="/trusted-by" element={<TrustedbyComponent />}/>
-    //   <Route path="/our-promises" element={<OurPromisesComponent />}/>
-    //   <Route path="/contact" element={<ContactSection />}/>
-    //   <Route path="/college" element={<College/>}/>
-    //   <Route path="*" element={<NotFound />}/>
-    // </Routes>
-    //  <div>
-    //   <UsestateformComponent />
+    <Routes>
+      {/* Main sections */}
+      <Route path="/" element={<RectangleBox />} />
+      <Route path="/galance" element={<Mycomponent />} />
+      <Route path="/trusted-by" element={<TrustedbyComponent />} />
+      <Route path="/our-promises" element={<OurPromisesComponent />} />
+      <Route path="/contact" element={<ContactSection />} />
+      <Route path="/college" element={<College />} />
 
-    // </div>
-    <div>
-      <Lms/>
-    </div>
-    // <Routes>
-    //   <Route path="/api-fetch" element={<FetchApiComponent />} />
-    //   <Route path="/use-effect-practice-2" element={<UseEffectPracticeTwo />} />
-    // </Routes>
-    
-       
+      {/* Practice pages */}
+      <Route path="/api-fetch" element={<FetchApiComponent />} />
+      <Route path="/use-effect-practice-2" element={<UseEffectPracticeTwo />} />
+
+      {/* 404 Page */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
-} 
+}
 
 export default App;
-
-
